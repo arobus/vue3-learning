4dd9c196b1f576b9db4c258d77f52481bbd5d625
Anurup Raveendran <anurup@robustest.com>
2020-11-06 06:44:31 +0530

    basic-event code

---
+++ app.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: '',
+      confirmedName: ''
+    };
+  },
+  methods: {
+      confirmName() {
+        this.confirmedName = this.name
+      },
+      setName(e, lastName){
+        this.name = e.target.value + ' ' + lastName
+      },
+      add(num) {
+          this.counter += num
+      },
+      reduce(num) {
+          this.counter -= num
+      },
+      submitForm(){          
+        alert('Submitted')
+      }
+  }
+});
+
+app.mount('#events');
