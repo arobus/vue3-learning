c63236bb7a9cd31fa8333e8505a2bb67f9bb73f2
Anurup Raveendran <anurup@robustest.com>
2020-12-28 12:37:19 +0530

    Dynamic Components

--- src/components/CourseGoals.vue
+++ src/components/CourseGoals.vue
@@ -1,17 +1,26 @@
 <template>
-    <ul>
-        <li v-for="goal in goals" :key="goal">
-            <slot :item="goal" another-prop="sfsdf"></slot>
-        </li>
-        
-    </ul>
+    <!-- using dynamic components -->
+    <!-- <keep-alive> we don't need keep alive since we are not using any inputs and don't need state to saved in case we switch  components -->
+    <!-- just keeping it here for knowing the syntax--> 
+        <component :is="selectedComponent"></component>
+    <!-- </keep-alive>     -->
+    <div style="display: flex">
+        <button @click="selectedComponent = 'goals-list'" :disabled="selectedComponent == 'goals-list'">Goals List</button>
+        <button @click="selectedComponent = 'manage-goals'" :disabled="selectedComponent == 'manage-goals'">Manage Goals</button>
+    </div>
 </template>
 
 <script>
+import GoalsList from './GoalsList.vue'
+import ManageGoals from './ManageGoals.vue'
 export default {
+    components: {
+        GoalsList,
+        ManageGoals
+    },
     data() {
         return {
-            goals : ["Do #1", "Do #2"]
+            selectedComponent: "goals-list"
         }
     }
 }
---
+++ src/components/ManageGoals.vue
@@ -0,0 +1,3 @@
+<template>
+    Dummy Component for managing goals. Doesn't do anything. Just to demonstrate dynamic components
+</template>
