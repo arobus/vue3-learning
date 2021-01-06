62a5918329079c37650578e15ae208465f21499e
Anurup Raveendran <anurupraveendran@gmail.com>
2021-01-07 04:57:58 +0530

    section-22 complete

---
+++ transitions-v3/app.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      isVisible: true
+    }
+  },
+  methods: {
+    toggleBox() {
+      this.isVisible = !this.isVisible;
+    }
+  },
+});
+
+
+app.mount("#app")
